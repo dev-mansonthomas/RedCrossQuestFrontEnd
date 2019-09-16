b2d0aa6f3b6f44094e5244fb69913cc00eaf21fe
Thomas Manson <dev.mansonthomas@gmail.com>
2019-09-16 13:11:13 +0200

    remove env files from github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { QueteursComponent } from './queteurs/queteurs.component';
+
+import { AngularFireModule } from '@angular/fire';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
+
+import { QueteursComponent } from './queteurs/queteurs.component';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faCoffee, faComments, faUsers } from '@fortawesome/free-solid-svg-icons';
+
+import { environment } from '../environments/environment';
 
 
 
@@ -37,6 +42,7 @@ import { DepartComponent } from './tronc-queteur/depart/depart.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my-app-name'),
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
